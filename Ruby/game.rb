c241998583468b4286f3f1c4000271b6182f3958
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-11-24 10:38:31 +0100

    Gemfile, more specs, rspec expect syntax, smaller methods

--- Ruby/game.rb
+++ Ruby/game.rb
@@ -1,10 +1,10 @@
 require 'set'
 
 class Game
-  attr_accessor :board
+  attr_accessor :cells
 
   def initialize(cells = [])
-    @board = Set.new(cells)
+    @cells = Set.new(cells)
   end
 
   def count_neighbours(cell)
@@ -15,43 +15,45 @@ class Game
     moves = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
 
     moves.each do |dx,dy|
-      count += 1 if @board.include?([x+dx, y+dy])
+      count += 1 if cells.include?([x+dx, y+dy])
     end
 
     count
   end
 
-  def nextgen!
-    environment = Set.new
-
-    moves = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 0], [0, 1], [1, -1], [1, 0], [1, 1]]
-
-    @board.each do |x,y|
-      moves.each do |dx,dy|
-        environment.add([x+dx, y+dy])
-      end
-    end
-
-    nextgen_board = Set.new
+  def next_generation!
+    next_generation_cells = Set.new
 
     environment.each do |cell|
       neighbours = count_neighbours(cell)
-      if @board.include?(cell) && [2, 3].include?(neighbours)
-        nextgen_board.add(cell)
-      elsif !@board.include?(cell) && neighbours == 3
-        nextgen_board.add(cell)
+      if cells.include?(cell) && [2, 3].include?(neighbours)
+        next_generation_cells.add(cell)
+      elsif !cells.include?(cell) && neighbours == 3
+        next_generation_cells.add(cell)
       end
     end
 
-    @board = nextgen_board
+    @cells = next_generation_cells
+  end
+
+  def environment
+    @environment = Set.new
+
+    cells.each do |x,y|
+      [x-1, x, x+1].product([y-1, y, y+1]).each do |cell|
+        @environment.add(cell)
+      end
+    end
+
+    @environment
   end
 end
 
 describe Game do
-  describe 'empty board' do
-    it 'has empty board at start' do
+  describe 'empty cells' do
+    it 'has empty cells at start' do
       game = Game.new
-      game.board.should be_empty
+      expect(game.cells).to be_empty
     end
   end
 
@@ -61,12 +63,12 @@ describe Game do
     end
 
     it 'has one cell at start' do
-      @game.board.size.should == 1
+      expect(@game.cells.size).to eq(1)
     end
 
     it 'has zero cells in next generation' do
-      @game.nextgen!
-      @game.board.size.should == 0
+      @game.next_generation!
+      expect(@game.cells).to be_empty
     end
   end
 
@@ -78,13 +80,35 @@ describe Game do
     end
 
     it 'has 3 cells at start' do
-      @game.board.size.should == 3
+      expect(@game.cells.size).to eq(3)
     end
 
     it 'has 3 cells in next generation' do
-      @game.nextgen!
-      @game.board.size.should == 3
+      @game.next_generation!
+      expect(@game.cells.size).to eq(3)
+    end
+  end
+
+  describe '6 cells (3x2)' do
+    before do
+      cells = [
+        [0,1], [1,1], [2,1],
+        [0,0], [1,0], [2,0]
+      ]
+      @game = Game.new(cells)
     end
 
+    it 'has 6 cells at start' do
+      expect(@game.cells.size).to eq(6)
+    end
+
+    it 'has 6 cells in next generation (2 dies, 2 comes alive)' do
+      @game.next_generation!
+      expect(@game.cells.size).to eq(6)
+      expect(@game.cells).to include([1,2])
+      expect(@game.cells).to_not include([1,1])
+      expect(@game.cells).to_not include([1,0])
+      expect(@game.cells).to include([1,-1])
+    end
   end
 end
